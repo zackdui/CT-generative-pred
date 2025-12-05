36a585b35a4488ccf60e0338b757879e712f0916
Zack Duitz <iduitz@gmail.com>
2025-12-04 20:38:45 -0500

    Updates to registration saving to run from scripts and save bad exams

---
+++ scripts/save_registrations.py
@@ -0,0 +1,71 @@
+# This file is used to write nifti files, registrations matrices and update parquet files accordingly.
+import json
+
+from CTFM.utils.config import load_config
+from CTFM.utils.custom_loggers import RegistrationLogger, merge_log_into_parquet_sequential
+from CTFM.data.processing import write_new_nifti_files, write_registration_matrices
+
+
+if __name__ == "__main__":
+    # Variables
+    save_nifti_files = False
+    update_parquet = False
+    write_registration = True
+    max_nifti_files_to_save = 5  # Set to None to process all files
+    logger_name_nifti_writing = "/data/rbg/users/duitz/CT-generative-pred/metadata/nifti_creation_log.jsonl"
+    registration_log_path = "/data/rbg/users/duitz/CT-generative-pred/metadata/train/registration_logv3.jsonl"
+    updated_output_parquet = "/data/rbg/users/duitz/CT-generative-pred/metadata/train/full_data_single_timepoints_updated.parquet"
+    bad_registration_log = "/data/rbg/users/duitz/CT-generative-pred/metadata/train/bad_registration_exams.jsonl"
+
+    # Config loading
+    path_yaml = "configs/paths.yaml"
+    base_paths = load_config(path_yaml)
+    full_data_parquet = base_paths.full_data_train_parquet
+    single_patient_parquet = base_paths.full_data_train_timelines_parquet
+    nifti_output_dir = base_paths.nifti_dir
+
+    # Logger setup
+    nifti_logger = RegistrationLogger(
+        logger_name_nifti_writing,
+        key_cols=["exam_id"],
+    )
+    logger_registration = RegistrationLogger(
+        registration_log_path,
+        key_cols=["exam_id"],
+    )
+
+    # Write new NIfTI files
+    if save_nifti_files:
+        updated_parquet = write_new_nifti_files(full_data_parquet, nifti_logger, nifti_output_dir, max_files=max_nifti_files_to_save)
+
+    # Write registration matrices
+    if write_registration:
+        print("Starting writing registration matrices...")
+        updated_parquet_registration = write_registration_matrices(
+            single_patient_parquet=single_patient_parquet,
+            all_data_parquet=full_data_parquet,
+            logger=logger_registration,
+            resample=True,
+            max_transform_nifti_files=0,
+            bad_exam_json=bad_registration_log,)
+        print("Finished writing registration matrices.")
+        print(f"Updated parquet with registrations saved to: {updated_parquet_registration}")
+
+    # Update the main parquet with the log
+    if update_parquet:
+        print("Starting merging registration log into parquet...")
+        merge_log_into_parquet_sequential(
+            base_parquet=full_data_parquet,
+            log_path=registration_log_path,
+            key_cols=["exam_id"],
+            output_parquet=updated_output_parquet,
+        )
+        print("Finished merging registration log into parquet.")
+
+    
+
+
+
+
+
+
--- src/CTFM/data/processing.py
+++ src/CTFM/data/processing.py
@@ -19,6 +19,41 @@ from .utils import pydicom_to_nifti, ants_crop_or_pad_like_torchio, nib_to_ants
 from CTFM.utils.config import load_config
 from CTFM.utils.custom_loggers import RegistrationLogger, merge_log_into_parquet_sequential
 
+def _log_skip(jsonl_path, exam_id, pid, error=None):
+    """"
+    Logs a skipped exam to a JSONL file with the exam ID, patient ID, and optional error message.
+    Helpful when processing large datasets and you need to track issues
+    """
+    entry = {
+        "exam_id": exam_id,
+        "pid": pid,
+        "error": str(error) if error else None,
+    }
+    with open(jsonl_path, "a") as f:
+        f.write(json.dumps(entry) + "\n")
+
+def _load_jsonl_to_lists(jsonl_path):
+    """
+    Loads a JSONL file and extracts exam IDs and patient IDs into separate lists.
+    """
+    exam_ids = []
+    pids = []
+    with open(jsonl_path, "r") as f:
+        for line in f:
+            if not line.strip():
+                continue
+            record = json.loads(line)
+            exam_ids.append(str(record["exam_id"]))
+            pids.append(str(record["pid"]))
+    return exam_ids, pids
+
+def _save_id_lists_pt(path, exam_ids, pids):
+    data = {
+        "exam_ids": exam_ids,
+        "pids": pids,
+    }
+    torch.save(data, path)
+
 def write_new_nifti_files(full_data_parquet, 
                           logger, 
                           nifti_output_dir: str = "/data/rbg/scratch/lung_ct/nlst_nifti/", 
@@ -60,12 +95,13 @@ def write_registration_matrices(single_patient_parquet,
                                 resample: bool = True,
                                 resampling_params: tuple = (0.703125 ,0.703125, 2.5),
                                 matrix_output_dir: str = "/data/rbg/scratch/lung_ct/registration_zack",
-                                save_transforms: bool = True, 
+                                save_transforms: bool = False, 
                                 transform_nifti_output_dir: str = "/data/rbg/scratch/lung_ct/nlst_nifti_transformed", 
-                                save_nifti: bool = True, 
+                                save_nifti: bool = False, 
                                 nifti_output_dir: str = "/data/rbg/scratch/lung_ct/nlst_nifti",
                                 max_nifti_files: int = 10,
-                                max_transform_nifti_files: int = 100):
+                                max_transform_nifti_files: int = 100,
+                                bad_exam_json: str = "./bad_registration_exams.jsonl"):
     """
     This function reads two parquet files: one containing single patient timelines and another
     containing all exam data. It performs image registration for patients with multiple exams,
@@ -109,6 +145,9 @@ def write_registration_matrices(single_patient_parquet,
         Maximum number of NIfTI files to create. If None, no limit.
     - max_transform_nifti_files: int or None
         Maximum number of transformed NIfTI files to save. If None, no limit.
+    - bad_exam_json: str
+        Path to a JSONL file to log bad exams that cannot be processed.
+        It must contain exam_id and pid fields.
 
     Returns:
     - str
@@ -117,7 +156,8 @@ def write_registration_matrices(single_patient_parquet,
     os.makedirs(matrix_output_dir, exist_ok=True)
     os.makedirs(transform_nifti_output_dir, exist_ok=True)
     os.makedirs(nifti_output_dir, exist_ok=True)
-
+    bad_exam_ids, bad_pids  = _load_jsonl_to_lists(bad_exam_json)
+    bad_pids_set = set(bad_pids)
     # Variables
     regular_nifti_count = 0
     transform_nifti_count = 0
@@ -147,6 +187,8 @@ def write_registration_matrices(single_patient_parquet,
     index_map = {eid: i for i, eid in enumerate(df_all_data["exam_id"])}
 
     for index, row in df_single_patient.iterrows():
+        if row["pid"] in bad_pids_set:
+            continue  # skip bad exams
         fixed_index = index_map[row["exam_id"][0]]
         fixed_index = int(fixed_index)
         fixed_exam_id = row["exam_id"][0]
@@ -179,6 +221,7 @@ def write_registration_matrices(single_patient_parquet,
             except HeaderDataError:
                 # *** SKIP THIS EXAM, PRINT ONLY pid + exam_id, CONTINUE ***
                 print(f"SKIP_BAD_NIFTI pid={full_data_exam_row['pid']} exam_id={full_data_exam_row['exam_id']}")
+                _log_skip(bad_exam_json, full_data_exam_row['exam_id'], full_data_exam_row['pid'], error="HeaderDataError during NIfTI conversion")
                 continue
             fixed_img = nib_to_ants(nifti_image)
             fixed_img = fixed_img.astype("float32")
@@ -258,6 +301,7 @@ def write_registration_matrices(single_patient_parquet,
                                                         )
                 except HeaderDataError:
                     print(f"SKIP_BAD_NIFTI pid={full_data_exam_row['pid']} exam_id={exam_id}")
+                    _log_skip(bad_exam_json, exam_id, full_data_exam_row['pid'], error="HeaderDataError during NIfTI conversion")
                     continue
                 moving_img = nib_to_ants(nifti_image)
                 moving_img = moving_img.astype("float32")
@@ -289,7 +333,26 @@ def write_registration_matrices(single_patient_parquet,
 
             rigid = ants.registration(fixed_img, moving_img, type_of_transform="Rigid")
             forward_path = os.path.join(matrix_output_dir, f"forward_f{fixed_exam_id}_m{exam_id}.mat")
+            fwd_tmp = rigid["fwdtransforms"][0]
             shutil.move(rigid["fwdtransforms"][0], forward_path)
+            if not os.path.exists(fwd_tmp):
+                print(
+                    f"SKIP_BAD_TRANSFORM pid={full_data_exam_row['pid']} "
+                    f"exam_id={exam_id} reason=missing_forward_tmp"
+                )
+                _log_skip(bad_exam_json, exam_id, full_data_exam_row['pid'], error="Missing temporary forward transform file")
+                continue
+
+            try:
+                shutil.move(fwd_tmp, forward_path)
+            except FileNotFoundError:
+                # Extremely defensive: if something still went wrong, skip this exam.
+                print(
+                    f"SKIP_BAD_TRANSFORM pid={full_data_exam_row['pid']} "
+                    f"exam_id={exam_id} reason=move_failed"
+                )
+                _log_skip(bad_exam_json, exam_id, full_data_exam_row['pid'], error="Failed to move temporary forward transform file")
+                continue
             
             df_all_data.at[moving_index, "registration_file"] = forward_path
             df_all_data.at[moving_index, "registration_exists"] = True
@@ -361,87 +424,3 @@ def build_dummy_fixed(row):
     )
     return dummy
 
-
-if __name__ == "__main__":
-    # Variables
-    nifti_output_dir = "/data/rbg/users/duitz/CT-generative-pred/test_data"
-    max_files = 5  # Set to None to process all files
-
-    # Config loading
-    path_yaml = "configs/paths.yaml"
-    base_paths = load_config(path_yaml)
-    full_data_parquet = base_paths.full_data_train_parquet
-    single_patient_parquet = base_paths.full_data_train_timelines_parquet
-
-    # Logger setup
-    logger = RegistrationLogger(
-        "/data/rbg/users/duitz/CT-generative-pred/test_data/nifti_test_creation_log.jsonl",
-        key_cols=["exam_id"],
-    )
-    logger_registration = RegistrationLogger(
-        "/data/rbg/users/duitz/CT-generative-pred/metadata/train/registration_logv2.jsonl",
-        key_cols=["exam_id"],
-    )
-
-    # # Write new NIfTI files
-    # updated_parquet = write_new_nifti_files(full_data_parquet, logger, nifti_output_dir, max_files=max_files)
-
-    # # Update the main parquet with the log
-    # merge_log_into_parquet_sequential(
-    #     base_parquet=full_data_parquet,
-    #     log_path= "/data/rbg/users/duitz/CT-generative-pred/test_data/nifti_test_creation_log.jsonl",
-    #     key_cols=["exam_id"],
-    #     output_parquet="/data/rbg/users/duitz/CT-generative-pred/test_data/updated.parquet",
-    # )
-
-    print("Starting writing registration matrices...")
-    update_parquet_registration = write_registration_matrices(
-        single_patient_parquet=single_patient_parquet,
-        all_data_parquet=full_data_parquet,
-        logger=logger_registration,
-        resample=True)
-    print("Finished writing registration matrices.")
-    print(f"Updated parquet with registrations saved to: {update_parquet_registration}")
-
-    # # Update the main parquet with the log
-    # print("Starting merging registration log into parquet...")
-    # merge_log_into_parquet_sequential(
-    #     base_parquet=full_data_parquet,
-    #     log_path=  "/data/rbg/users/duitz/CT-generative-pred/metadata/train/registration_log.jsonl",
-    #     key_cols=["exam_id"],
-    #     output_parquet="/data/rbg/users/duitz/CT-generative-pred/metadata/train/full_data_single_timepoints_updated.parquet",
-    # )
-    # print("Finished merging registration log into parquet.")
-
-    
-
-
-
-
-
-
-
-
-    # Testing code
-    # first = pd.read_parquet("/data/rbg/users/duitz/CT-generative-pred/test_data/updated.parquet")
-    # second = pd.read_parquet(full_data_parquet)
-    # print(f"Length of first: {len(first)}, length of second: {len(second)}")
-    # # Locate exam with exam_id 10000402215824639 and print the row for both dataframes
-    # exam_id_to_check = "10000402215824639"
-    # print("Row in first dataframe:")
-    # print(first[first["exam_id"] == exam_id_to_check]["sorted_paths"])
-    # print("Row in second dataframe:")
-    # print(second[second["exam_id"] == exam_id_to_check]["sorted_paths"])
-    # print("Row in first dataframe:")
-    # print(first[first["exam_id"] == exam_id_to_check]["nifti_path"])
-    # print("Row in second dataframe:")
-    # print(second[second["exam_id"] == exam_id_to_check]["nifti_path"])
-
-    # print("Base dtypes:")
-    # print(first["exam_id"].dtypes)
-
-    # print("\nLog dtypes:")
-    # print(second["exam_id"].dtypes)
-
-    # print("same object?:", first is second)
-    # print("same content?:", first.equals(second))
