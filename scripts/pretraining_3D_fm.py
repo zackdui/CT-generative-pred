dfd78badfc3f2aa4d30180b76518a9e3c0442f80
Zack Duitz <iduitz@gmail.com>
2026-02-12 14:50:12 -0500

    updated version of pretraining 3d nodules

--- scripts/pretraining_3D_fm.py
+++ scripts/pretraining_3D_fm.py
@@ -1,6 +1,7 @@
 # This file is used to run training of 3D diffusion model on unconditional
 import os
 import sys
+import shutil
 from monai.networks.nets import DiffusionModelUNet
 # from generative.networks.nets import DiffusionModelUNet
 from torch.utils.data import DataLoader
@@ -11,7 +12,7 @@ from lightning.pytorch.loggers import CSVLogger, WandbLogger
 from lightning.pytorch.strategies import DDPStrategy
 from torchinfo import summary
 
-sys.path.insert(0, "/data/rbg/users/duitz/VAE3d/src")
+# sys.path.insert(0, "/data/rbg/users/duitz/VAE3d/src")
 from vae3d2d import CustomVAE
 
 # This repo imports
@@ -34,6 +35,7 @@ def main(training_args,
          val_dataset, 
          test_dataset, 
          run_output_dir, 
+         decode_model=None,
          single_image=False):
 
     debug_flag = training_args.debug_flag
@@ -50,15 +52,29 @@ def main(training_args,
     val_loader = DataLoader(val_dataset, batch_size=train_batch_size, shuffle=True, num_workers=2, collate_fn=collate_image_meta)
     test_loader = DataLoader(test_dataset, batch_size=test_batch_size, shuffle=False, collate_fn=collate_image_meta)
 
+    import time, torch
+    it = iter(train_loader)
+    torch.cuda.synchronize()
+    t0 = time.time()
+    batch = next(it)
+    t1 = time.time()
+    print("batch fetch:", t1-t0)
+
     ####### For Single Image Training ########
     if single_image:
         image = train_dataset[0][0]
         meta_data = train_dataset[0][1]
-        repeat_count = 10
+        repeat_count = 20000
         train_dataset = RepeatedImageDataset(image, repeat_count, meta_data=meta_data)
         val_dataset = RepeatedImageDataset(image, 4, meta_data=meta_data)
-        train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True, num_workers=0, collate_fn=collate_image_meta)
-        val_loader = DataLoader(val_dataset, batch_size=1, shuffle=False, num_workers=0, collate_fn=collate_image_meta)
+        train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True, num_workers=0, collate_fn=collate_image_meta)
+        val_loader = DataLoader(val_dataset, batch_size=2, shuffle=False, num_workers=0, collate_fn=collate_image_meta)
+        if decode_model is not None:
+            decode_model.eval()
+            with torch.no_grad():
+                decode_model = decode_model.to('cuda')
+                out_image = decode_model.decode(image.unsqueeze(0).to(next(decode_model.parameters()).device))
+                image = out_image.squeeze(0)
         reverse_normalized_image = reverse_normalize(image.cpu())
         image_to_save = window_ct_hu_to_png(reverse_normalized_image, center=-600.0, width=1500.0, bit_depth=8)
         single_image_output_dir = os.path.join(run_output_dir, "single_image_example")
@@ -73,21 +89,35 @@ def main(training_args,
     torch.backends.cudnn.allow_tf32 = True
     torch.set_float32_matmul_precision("high")
 
-    model = DiffusionModelUNet(spatial_dims=unet_3d_cfm_configs.spatial_dims,
-                           in_channels=unet_3d_cfm_configs.in_channels,
-                           out_channels=unet_3d_cfm_configs.out_channels,
-                           num_res_blocks=unet_3d_cfm_configs.num_res_blocks,
-                           channels=unet_3d_cfm_configs.channels,
-                           resblock_updown=unet_3d_cfm_configs.resblock_updown,
-                           attention_levels=unet_3d_cfm_configs.attention_levels,
-                           num_head_channels=unet_3d_cfm_configs.num_head_channels,
-                           norm_num_groups=unet_3d_cfm_configs.norm_num_groups,
-                           use_flash_attention=True)
+    # model = DiffusionModelUNet(spatial_dims=unet_3d_cfm_configs.spatial_dims,
+    #                        in_channels=unet_3d_cfm_configs.in_channels,
+    #                        out_channels=unet_3d_cfm_configs.out_channels,
+    #                        num_res_blocks=unet_3d_cfm_configs.num_res_blocks,
+    #                        channels=unet_3d_cfm_configs.channels,
+    #                        resblock_updown=unet_3d_cfm_configs.resblock_updown,
+    #                        attention_levels=unet_3d_cfm_configs.attention_levels,
+    #                        num_head_channels=unet_3d_cfm_configs.num_head_channels,
+    #                        norm_num_groups=unet_3d_cfm_configs.norm_num_groups,
+    #                        use_flash_attention=True)
 
-    model_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
-    print(f"Number of trainable parameters in model: {model_parameters}")
+    # model_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    # print(f"Number of trainable parameters in model: {model_parameters}")
 
-    light_model = UnetLightning3D(model, lr=lr, output_dir=run_output_dir, input_channels=1)
+    unet_kwargs = dict(
+            spatial_dims=unet_3d_cfm_configs.spatial_dims,
+            in_channels=unet_3d_cfm_configs.in_channels,
+            out_channels=unet_3d_cfm_configs.out_channels,
+            num_res_blocks=unet_3d_cfm_configs.num_res_blocks,
+            channels=unet_3d_cfm_configs.channels,
+            resblock_updown=unet_3d_cfm_configs.resblock_updown,
+            attention_levels=unet_3d_cfm_configs.attention_levels,
+            num_head_channels=unet_3d_cfm_configs.num_head_channels,
+            norm_num_groups=unet_3d_cfm_configs.norm_num_groups,
+            use_flash_attention=True,
+        )
+
+    # light_model = UnetLightning3D(model, lr=lr, output_dir=run_output_dir, input_channels=1)
+    light_model = UnetLightning3D(DiffusionModelUNet, unet_kwargs, lr=lr, output_dir=run_output_dir, input_channels=unet_3d_cfm_configs.in_channels, decode_model=decode_model, img_size=training_args.img_size)
     
     if debug_flag:
         torchinfo_summary = summary(light_model, input_size=[(8, 1, 32, 128, 128), (8,)])
@@ -98,7 +128,7 @@ def main(training_args,
     wandb_logger = WandbLogger(
                                 project=training_args.wandb_project,
                                 name=training_args.wandb_run_name,
-                                config=unet_3d_cfm_configs.__dict__ | training_args.__dict__ | {'model_parameters': model_parameters},
+                                config=unet_3d_cfm_configs.__dict__ | training_args.__dict__,
                             )
     
     checkpointer_train = ModelCheckpoint(
@@ -115,14 +145,14 @@ def main(training_args,
         filename='val-{epoch}-{val_loss:.2f}',
         save_top_k=1,
         mode="min",
-        save_last=True,
+        save_last=False,
     )
     
     lr_monitor = LearningRateMonitor(logging_interval='step')
 
     pl.seed_everything(42, workers=True)
     if debug_flag:
-        trainer = pl.Trainer(logger=[csv_logger, wandb_logger],
+        trainer = pl.Trainer(logger=[csv_logger, wandb_logger], 
                         devices=3, 
                         deterministic=False, 
                         accelerator="auto",
@@ -133,28 +163,30 @@ def main(training_args,
                         gradient_clip_val=1.0, 
                         gradient_clip_algorithm="value",
                         strategy=DDPStrategy(),) 
-    else:           
+    else:  
+        # from lightning.pytorch.profilers import SimpleProfiler, AdvancedProfiler         
         trainer = pl.Trainer(logger=[csv_logger, wandb_logger],
-                            devices=1, 
+                            devices=8, 
                             deterministic=False, 
+                            # accumulate_grad_batches=4,
                             accelerator="auto",
                             callbacks=[checkpointer_train, checkpointer_val, lr_monitor], 
-                            log_every_n_steps=20,
-                            max_epochs=20,
+                            log_every_n_steps=50,
+                            max_epochs=500,
                             num_sanity_val_steps=2,
                             gradient_clip_val=1.0, 
-                            gradient_clip_algorithm="value",
+                            gradient_clip_algorithm="norm",
                             strategy=DDPStrategy(),
                             check_val_every_n_epoch=1,
-                            limit_val_batches=1,
-                            profiler="simple",
+                            limit_val_batches=10,
+                            # profiler=AdvancedProfiler(dirpath=".", filename="pl_profiler.txt"), #"advanced",
                             precision="bf16-mixed") # I only want it to validate one batch each time
                        
     trainer.fit(model=light_model, train_dataloaders=train_loader, val_dataloaders=val_loader)
 
-    best_path = checkpointer_val.best_model_path
+    # best_path = checkpointer_val.best_model_path
 
-    trainer.test(dataloaders=test_loader, ckpt_path=best_path)
+    # trainer.test(dataloaders=test_loader, ckpt_path=best_path)
     
     if trainer.is_global_zero:
         log_dir = trainer.logger.log_dir
@@ -162,21 +194,24 @@ def main(training_args,
         fig = plot_lr_from_metrics(path, show=False)
         fig2 = plot_loss_from_metrics(path, show=False)
 
-def create_datasets(base_paths, max_examples, encode_model=None):
+def create_datasets(base_paths, max_examples, encode_model=None, encoded_data=False):
     train_data_parquet = base_paths.full_data_train_parquet
-    train_nodule_parquet = base_paths.bounding_boxes_train_parquet
-    raw_nodule_index = base_paths.raw_nodule_index
-    data_root = base_paths.raw_cached_nodule_dir
-
     val_data_parquet = base_paths.full_data_val_parquet
-    val_nodule_parquet = base_paths.bounding_boxes_val_parquet
-
     test_data_parquet = base_paths.full_data_test_parquet
+
+    train_nodule_parquet = base_paths.bounding_boxes_train_parquet
+    val_nodule_parquet = base_paths.bounding_boxes_val_parquet
     test_nodule_parquet = base_paths.bounding_boxes_test_parquet
+    if not encoded_data:   
+        nodule_index = base_paths.raw_nodule_index
+        data_root = base_paths.raw_cached_nodule_dir
+    else:
+        nodule_index = base_paths.encoded_nodule_index
+        data_root = base_paths.encoded_cached_nodule_dir
 
     train_dataset = CachedNoduleDataset(train_data_parquet, 
                                         train_nodule_parquet, 
-                                        raw_nodule_index, 
+                                        nodule_index, 
                                         data_root, 
                                         split="train",
                                         max_length=num_samples,
@@ -185,7 +220,7 @@ def create_datasets(base_paths, max_examples, encode_model=None):
     
     val_dataset = CachedNoduleDataset(val_data_parquet, 
                                       val_nodule_parquet, 
-                                      raw_nodule_index, 
+                                      nodule_index, 
                                       data_root, 
                                       split="val",
                                       max_length=num_samples,
@@ -194,12 +229,13 @@ def create_datasets(base_paths, max_examples, encode_model=None):
     
     test_dataset = CachedNoduleDataset(test_data_parquet, 
                                        test_nodule_parquet, 
-                                       raw_nodule_index, 
+                                       nodule_index, 
                                        data_root, 
                                        split="test",
                                        max_length=5,
                                        max_cache_size=10,
                                        return_meta_data=True)
+
     
     class EncodedDataset(torch.utils.data.Dataset):
         def __init__(self, base_ds, encoder=None, device="cuda"):
@@ -227,30 +263,43 @@ def create_datasets(base_paths, max_examples, encode_model=None):
     return train_dataset, val_dataset, test_dataset
 
 if __name__ == "__main__":
-    single_image = True  # set to True to train on a single image repeated
-    experiment_name = "test_more_steps" #"testing_functionality2"
+    single_image = False  # set to True to train on a single image repeated
     path_yaml = "configs/paths.yaml"
     training_configs = "configs/fm_3d_pretraining.yaml"
     unet_3d_cfm = "configs/unet_3d_cfm.yaml"
-    encode_data = False
-    encode_model_checkpoint = "/data/rbg/users/duitz/CT-generative-pred/final_saved_models/ae3d_16_3down.pt"
-    
+    encode_data = True
+    # encode_model_checkpoint = "/data/rbg/users/duitz/CT-generative-pred/final_saved_models/vae_fixed_std_no_reg.pt"
+    # if the checkpoint is None but encoded_data is True it will used the cached encoded data
+    encode_model_checkpoint = None
+    decode_model_checkpoint = "/data/rbg/users/duitz/CT-generative-pred/final_saved_models/vae_fixed_std_no_reg.pt"
 
     base_paths = load_config(path_yaml)
     training_args = load_config(training_configs)
 
     num_samples = training_args.num_samples
+    experiment_name = training_args.experiment_name
 
     experiment_dir = base_paths.pretraining_3d_fm_experiment_dir
     run_output_dir = os.path.join(experiment_dir, experiment_name)
     os.makedirs(run_output_dir, exist_ok=True)
 
-    if encode_data:
-        encode_model = CustomVAE.load_from_checkpoint(encode_model_checkpoint, map_location="cuda")
-    else:
-        encode_model = None
+    # Copy over config file
+    unet_config_copy_dst = os.path.join(run_output_dir, f"{experiment_name}_unet_config.yaml")
+    train_config_copy_dst = os.path.join(run_output_dir, f"{experiment_name}_train_config.yaml")
+    shutil.copy2(unet_3d_cfm, unet_config_copy_dst)
+    shutil.copy2(training_configs, train_config_copy_dst)
 
-    train_dataset, val_dataset, test_dataset = create_datasets(base_paths, num_samples, encode_model)
-    
-    main(training_args, unet_3d_cfm, train_dataset, val_dataset, test_dataset, run_output_dir, single_image=single_image)
+    if encode_model_checkpoint is not None:
+        model = CustomVAE.load_from_checkpoint(encode_model_checkpoint, map_location="cuda")
+        decode_model = model
+    elif encode_data:
+        decode_model = CustomVAE.load_from_checkpoint(decode_model_checkpoint, map_location="cpu")
+        model = None
+    else:
+        model = None
+        decode_model = None
+
+    train_dataset, val_dataset, test_dataset = create_datasets(base_paths, num_samples, model, encoded_data=encode_data)
+
+    main(training_args, unet_3d_cfm, train_dataset, val_dataset, test_dataset, run_output_dir, decode_model=decode_model, single_image=single_image)
 
