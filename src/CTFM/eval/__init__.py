ad12d12b62adc17eee6c9c1b71445ceba4dcef9a
Zack Duitz <iduitz@gmail.com>
2026-02-17 19:23:47 -0500

    eval sampler added to init

--- src/CTFM/eval/__init__.py
+++ src/CTFM/eval/__init__.py
@@ -1,7 +1,7 @@
 
 from .segmenter import load_segmentation_models, patch_segmenter, load_lung_model, patch_segmenter
 from .segmentation_pipeline import nnUNet, nnUNetConfidence, min_max_normalize_batch, apply_windowing
-from .utils import ImageEvaluatorPrep, get_volumes, visualize_segmentation, scale_bbox_after_interpolate, save_montage_with_bbox3d, dice_score
+from .utils import ImageEvaluatorPrep, get_volumes, visualize_segmentation, scale_bbox_after_interpolate, save_montage_with_bbox3d, dice_score, sample_euler
 
 __all__ = [
     "load_segmentation_models",
@@ -17,5 +17,6 @@ __all__ = [
     "scale_bbox_after_interpolate",
     "apply_windowing",
     "save_montage_with_bbox3d",
-    "dice_score"
+    "dice_score",
+    "sample_euler"
 ]
